0cf1b78fd419d82955606a9677447fd0ed066477
Annie T <36142304+atheriot@users.noreply.github.com>
2018-02-28 17:43:57 -0800

    Update kernel.c

--- phase3/kernel.c
+++ phase3/kernel.c
@@ -34,7 +34,7 @@ void InitKernelData(void) {        // init kernel data
 void InitKernelControl(void) {     // init kernel control 
 	current_time=0;
 	video_sem.val = 1;
-	video_sem->wait_q.size=0;
+	video_sem.wait_q.size=0;
 	
 	IDT_p = get_idt_base(); //get IDT location
 	//show: "IDT located at DRAM addr %x (%d).\n" (both address of IDT)
