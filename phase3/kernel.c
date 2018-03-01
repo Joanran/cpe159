5a380e499b1513f1d2d8f1b9cf42397611edb49c
Annie T <36142304+atheriot@users.noreply.github.com>
2018-03-01 11:50:08 -0800

    Update kernel.c

--- phase3/kernel.c
+++ phase3/kernel.c
@@ -17,7 +17,7 @@ int run_pid;                       // currently running PID; if -1, none selecte
 pid_q_t ready_pid_q, avail_pid_q;  // avail PID and those ready to run
 pcb_t pcb[PROC_NUM];               // Process Control Blocks
 char proc_stack[PROC_NUM][PROC_STACK_SIZE]; // process runtime stacks
-semaphore_t video_sum;			// Phase 3
+semaphore_t video_sem;			// Phase 3
 
 void InitKernelData(void) {        // init kernel data
   	int i; //, ret;
