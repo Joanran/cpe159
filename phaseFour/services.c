b0d8453f8170cfd7383582317f319b93137966a8
Annie T <36142304+atheriot@users.noreply.github.com>
2018-03-07 14:24:17 -0800

    Updated services.c

--- phaseFour/services.c
+++ phaseFour/services.c
@@ -139,20 +139,22 @@ void SempostService(int sem_num) {
 void TermService(int which) {
       int i, pid;
 
-      if 1st character of dsp buffer is null, return; // nothing to dsp
+      if(dsp[0]==NULL) return;	//if 1st character of dsp buffer is null, return; // nothing to dsp
 
       outportb(term[which].port, term[which].dsp[0]); // disp 1st char
 
-      conduct a loop, one by one {:
-         move each character in dsp buffer forward by 1 character
-         if encounter moving a NULL character, break loop
+     for(i=0; i<BUFF_SIZE; i++) {	// conduct a loop, one by one {:
+         //move each character in dsp buffer forward by 1 character
+         if(dsp[i]=='\0') {//if encounter moving a NULL character, break loop
+		break;
+	 }
       }
 
-      if 1st char of dsp buffer is null and the wait queue has PID {
+      if((dsp[0]==NULL) && ) { //if 1st char of dsp buffer is null and the wait queue has PID
          // str ends & there's a waiter
          // release the 1st waiter in the wait queue:
-            1. dequeue it from the wait queue
-            2. update its state
-            3. enqueue it to ready PID queue
+            DeQ(&dsp[i].dsp_wait_q);	//1. dequeue it from the wait queue
+            pcb[run_pid].state=READY;	//2. update its state
+            EnQ(run_pid, &ready_pid_q);	//3. enqueue it to ready PID queue
       }
    }
