d92f4138584a03fff55445bf41818999c09e1834
Annie T <36142304+atheriot@users.noreply.github.com>
2018-03-07 13:34:37 -0800

    Updated proc.c

--- phaseFour/proc.c
+++ phaseFour/proc.c
@@ -20,29 +20,23 @@ void IdleProc(void) {
 }
 
 void UserProc(void) {
-   int i, my_pid, centi_sec;
-   char str[] = "   "; // either "   " or { ' ', ' ', ' ', '\0' }
+      int my_pid, centi_sec, which;
+      char str[] = "   ";
 
-   my_pid = sys_getpid();
-   centi_sec = 50 * my_pid;
+      my_pid = sys_getpid();
+      centi_sec = 50 * my_pid;
+      str[0] = '0' + my_pid/10;
+      str[1] = '0' + my_pid%10;
 
-   str[0] = '0' + my_pid/10;
-   str[1] = '0' + my_pid%10;
+      which = (my_pid % 2)? TERM1 : TERM2;
 
-   while(1) {
-     // sys_write(STDOUT, str, 3);   // show my PID
-     // sys_sleep(centi_sec);        // sleep for .5 sec x PID
-      
-      //new stuff below
-      sys_semwait(STDOUT);                      // if commented out?
-      sys_write(STDOUT, str, 3);
-      for(i=0; i<LOOP/2; i++) asm("inb $0x80"); // burn CPU time
-      sys_write(STDOUT, "using ", 6);
-      for(i=0; i<LOOP/2; i++) asm("inb $0x80");
-      sys_write(STDOUT, "the ", 4);
-      for(i=0; i<LOOP/2; i++) asm("inb $0x80");
-      sys_write(STDOUT, "video... ", 9);
-      sys_sempost(STDOUT);                      // if commented out?
-      sys_sleep(centi_sec);                     // sleep PID x .5 sec
+      while(1) {
+         sys_write(which, str, 3);   // show my PID
+         sys_write(which, "is ", 3);
+         sys_write(which, "using ", 6);
+         sys_write(which, "terminal", 8);
+         sys_write(which, "...\n\r", 5);
+
+         sys_sleep(centi_sec);        // sleep for .5 sec x PID
+      }
    }
-}
