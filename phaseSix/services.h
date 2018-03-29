2d6b1bb5584f5c3e4d416443f836513990ead8be
Annie T <36142304+atheriot@users.noreply.github.com>
2018-03-28 18:33:22 -0700

    Add files via upload

---
+++ phaseSix/services.h
@@ -0,0 +1,28 @@
+// services.h, 159
+
+#ifndef _SERVICES_H_
+#define _SERVICES_H_
+
+#include "kernel_types.h"   // need definition of 'func_p_t' below
+
+void NewProcService(func_p_t);
+void TimerService(void);
+
+void SyscallService(trapframe_t *);
+void SleepService(int);
+void GetpidService(int *);
+void WriteService(int,char *,int);
+
+//phase three below
+void SemWaitService(int);
+void SempostService(int);
+
+//phase four below
+void TermService(int);
+
+//phase five below
+void ReadService(int,char *,int);
+void DspService(int);
+void KbService(int);
+
+#endif
