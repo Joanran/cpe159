47cab4e0542b7782fcf7c4a1d78254f41286d4dd
Annie T <36142304+atheriot@users.noreply.github.com>
2018-04-02 18:14:24 -0700

    Updated tools.c
    Took out the returns and make two into a void

--- phase6/tools.c
+++ phase6/tools.c
@@ -21,24 +21,22 @@ void MyMemcpy(char *dst, char *src, int bytes) {
 	}
 }
 
-char* MyStrAppend(char* str, char ch) {
+void MyStrAppend(char* str, char ch) {
     while(*str){
         str++;
     }
     *str = ch;
     str++;
     *str='\0';
-    return str;
 }
 
-char* MyStrMove(char *str) {
+void MyStrMove(char *str) {
     char* temp = str;
     while (*str) {
 	    temp++;
 	    *str = *temp;
 	    str++;
     }
-    return str;
 }
 
 // clear DRAM by setting each byte to zero
