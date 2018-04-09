5b03d3eae8c9d8a4e573f3367fd3d70873947e4a
erikgonzalez <36181412+erikgonzalez@users.noreply.github.com>
2018-04-08 21:01:29 -0700

    Add files via upload

---
+++ phase7/syscalls.h
@@ -0,0 +1,16 @@
+//syscalls.h
+
+#ifndef _SYSCALLS_H_
+#define _SYSCALLS_H_
+
+#include "kernel_constants.h"
+int sys_fork(void);
+int sys_getpid(void); 
+void sys_write(int, char *, int);
+void sys_read(int, char *, int);
+void sys_sleep(int);
+
+void sys_semwait(int);
+void sys_sempost(int); 
+
+#endif
