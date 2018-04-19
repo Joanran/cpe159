f9a1a2e9ad2a2c274b106e78398af88cd65460dd
Annie T <36142304+atheriot@users.noreply.github.com>
2018-04-18 18:45:30 -0700

    Copy of phase 7

---
+++ phase8/services.h
@@ -0,0 +1,35 @@
+// services.h, 159
+
+#ifndef _SERVICES_H_
+#define _SERVICES_H_
+
+#include "kernel_types.h"   // need definition of 'func_p_t' below
+
+void NewProcService(func_p_t);
+void TimerService(void);
+
+void SyscallService(trapframe_t *);
+void SleepService(int);
+void GetpidService(int *);
+void WriteService(int,char *,int);
+
+//phase three below
+void SemWaitService(int);
+void SempostService(int);
+
+//phase four below
+void TermService(int);
+
+//phase five below
+void ReadService(int,char *,int);
+void DspService(int);
+void KbService(int);
+
+//phase six below
+void ForkService(int *);
+
+// phase seven below
+void SignalService(int, func_p_t);
+void WrapperService(int, func_p_t);
+void GetPpidService(int *);
+#endif
