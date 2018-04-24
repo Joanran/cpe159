5f256ae60e2b4156d1cc8fdd498507f784c64edc
Annie T <36142304+atheriot@users.noreply.github.com>
2018-04-24 12:27:22 -0700

    Updated kernel_constants.h

--- phase9/kernel_constants.h
+++ phase9/kernel_constants.h
@@ -40,4 +40,9 @@
  #define SYS_EXIT 1
  #define SYS_WAITCHILD 7
 
+#define SYS_EXEC 11         (new service #)
+#define PAGE_BASE 0xe00000  (MyOS.dli ends at byte 14M-1, 0xdfffff)
+#define PAGE_NUM 20         (only 20 DRAM pages to experiment)
+#define PAGE_SIZE 4096      (each DRAM page is 4KB in size)
+
 #endif
