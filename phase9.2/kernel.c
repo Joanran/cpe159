98ca180213c161752cc658bfb0c75310a2f8968f
Annie T <36142304+atheriot@users.noreply.github.com>
2018-05-12 11:42:37 -0700

    Initialized OS_TT

--- phase9.2/kernel.c
+++ phase9.2/kernel.c
@@ -52,6 +52,8 @@ void InitKernelControl(void) {     // init kernel control
 	video_sem.val = 1;
 	video_sem.wait_q.size=0;
 	
+	OS_TT = get_cr3(); 
+	
 	IDT_p = get_idt_base(); //get IDT location
 	//show: "IDT located at DRAM addr %x (%d).\n" (both address of IDT)
 	cons_printf("IDT located at DRAM addr %x (%d). \n", IDT_p, IDT_p );
